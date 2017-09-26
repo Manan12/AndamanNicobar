c9763a968eceeb9b3cc130dcb6f13fdb2e51dac9
Manan12 <manan@mokko.io>
2017-09-26 17:24:08 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var App = angular.module('App', []);
+
+App.controller('mainCtrl', function($scope, $http) {
+  $http.get('data.json')
+       .then(function(res){
+          $scope.data = res.data;
+        });
+});
